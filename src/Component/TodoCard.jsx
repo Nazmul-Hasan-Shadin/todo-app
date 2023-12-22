5a06fe1d4769969183b1a4b92332d5bfb3af926f
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-12-22 15:58:06 +0600

    delete functionality has intrigated

--- src/Component/TodoCard.jsx
+++ src/Component/TodoCard.jsx
@@ -1,56 +1,83 @@
-import React from 'react';
-import useAxiosPublic from '../hook/useAxiosPublic';
-import toast from 'react-hot-toast';
-import { useDrag } from 'react-dnd';
+import React from "react";
+import useAxiosPublic from "../hook/useAxiosPublic";
+import toast from "react-hot-toast";
+import { useDrag } from "react-dnd";
+import { FaTrash } from "react-icons/fa";
 
-const TodoCard = ({todo,refetch}) => {
-   
-  const [{isDragging},drag]= useDrag(()=>({
-    type:"card",
-    item:{id:todo}
-    ,
-    collect:(monitor)=>({
-      isDragging: !!monitor.isDragging()
-    })
-  }))
+const TodoCard = ({ todo, refetch }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "card",
+    item: { id: todo },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
 
+  console.log(todo);
 
-    console.log(todo);
-    const axiosPublic= useAxiosPublic()
-    const handleUpdateStatus=async(id)=>{
-         
-
-       
-       try {
-         const res= await axiosPublic.patch(`/update-todo/${id}`,{status:'completed'})
-          if (res.data.modifiedCount>0) {
-              toast.success('Todo has Completed')
-              refetch()
-          }
-
-       } catch (error) {
-        console.log(error);
-       }
+  // ===================update todo status by patch =================================
 
 
+  const axiosPublic = useAxiosPublic();
+  const handleUpdateStatus = async (id) => {
+    try {
+      const res = await axiosPublic.patch(`/update-todo/${id}`, {
+        status: "completed",
+      });
+      if (res.data.modifiedCount > 0) {
+        toast.success("Todo has Completed");
+        refetch();
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div ref={drag} className="card w-[400px] bg-primary mt-12 text-primary-content">
-        <div className="card-body p-2 pl-7">
+     // ===================update todo status  end=================================
 
+    
+     const handleDeleteTodo=(id)=>{
+        
+           axiosPublic.delete(`/todo-delete/${id}`)
+           .then(res=> {
+             console.log(res);
+             refetch()
+             toast.success('Deleted Successful')
+           })
+           .catch(err=>console.log(err))
+     }
+
+
+
+
+  return (
+    <div
+      ref={drag}
+      className="card w-[400px] bg-primary mt-12 text-primary-content"
+    >
+      <div className="card-body p-2 pl-7">
+        <span className="flex justify-between">
           <h2 className="card-title"> {todo?.title} </h2>
-          <p> {todo?.desc} </p>
-          <div className=" justify-end">
-          <div className='flex justify-between'>
-          <p> DeadLine: {todo?.deadLine} </p>
-         <button onClick={()=>handleUpdateStatus(todo._id)} className='btn'>  {todo?.status==='completed'?'completed':'complete'} </button>
+          <FaTrash onClick={()=>handleDeleteTodo(todo?._id)} />
+        </span>
+
+        <p> {todo?.desc} </p>
+        <div className=" justify-end">
+          <div className="flex justify-between">
+            <p> DeadLine: {todo?.deadLine} </p>
+            <button
+              onClick={() => handleUpdateStatus(todo._id)}
+              className="btn"
+            >
+              {" "}
+              {todo?.status === "completed" ? "completed" : "complete"}{" "}
+            </button>
           </div>
-           <p>  </p>
-          </div>
+          <p> </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default TodoCard;
